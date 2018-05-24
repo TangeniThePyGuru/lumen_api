32f202db2738d438abdd5943e1cdbc9168827f8c
Tangeni <tangenishikomba@gmail.com>
2018-05-24 15:24:52 +0200

    seeders created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+    	\Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+		\App\Teacher::truncate();
+		\App\Student::truncate();
+		\App\Course::truncate();
+
+//		truncate the pivot table
+		\Illuminate\Support\Facades\DB::table('course_student')->truncate();
+
+		factory(\App\Teacher::class, 50)->create();
+//		dd('testing');
+		factory(\App\Student::class, 500)->create();
+
+		factory(\App\Course::class, 40)->create()->each(function (\App\Course $course){
+			$course->students()->attach(array_rand(range(1, 500),40));
+		});
+
         // $this->call('UsersTableSeeder');
     }
 }

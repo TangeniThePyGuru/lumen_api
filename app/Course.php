285588702dadb359f488dd823756c8b34cb99acf
Tangeni Shikomba <tangenishikomba@gmail.com>
2018-06-16 18:36:53 +0100

    rules variable added

--- app/Course.php
+++ app/Course.php
@@ -14,6 +14,14 @@ class Course extends Model
 	 * @var array
 	 */
 	protected $hidden = ['create_at', 'updated_at'];
+    /**
+     * @var array
+     */
+    public static $rules = [
+        'title' => 'required',
+        'description' => 'required',
+        'value' => 'required|numeric'
+    ];
 
 	public function students(){
 		return $this->belongsToMany(Student::class);
